2af973566169eabbb907a161845bba05a30a2672
Gergo Karcagi <karcagigergo@gmail.com>
2022-01-20 16:39:50 -0300

    activerecord-import gem setup with the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,16 @@
 class User < ApplicationRecord
+  require 'csv'
+  require 'activerecord-import/base'
+  require 'activerecord-import/active_record/adapters/postgresql_adapter'
+
   validates :name, :password, presence: true
-  validates :password, length: { in: 10..16 }
   validates :password, format: { with: /\A(?!.*(.)\1\1)(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{10,16}\z/ }
+
+  def self.my_import(file)
+    users = []
+    CSV.each(file.path, headers: true) do |row|
+      users << User.new(row.to_h)
+    end
+    User.import users, recursive: true
+  end
 end
