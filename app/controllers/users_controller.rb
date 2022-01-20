845d6f97ee9d6d9da0a2321316fccfc2c7aed764
Gergo Karcagi <karcagigergo@gmail.com>
2022-01-20 16:52:59 -0300

    created controller for user, updated routes and generated a basic view for the uploaded file to be shown

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def import
+    User.my_import(params[:file])
+    redirect_to root_path, notice: "Successfully imported User data!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root to: "users#index"
   get 'users/index'
-  get 'users/import'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/import' => 'users#my_import'
+  
+  resources :users do
+    collection {post :import}
+  end
 end
