d79212070dedd0eb702ef4354c71252e87a8a0a9
Gergo Karcagi <karcagigergo@gmail.com>
2022-01-20 16:22:38 -0300

    added tests for user validation and added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, :password, presence: true
+  validates :password, length: { in: 10..16 }
+  validates :password, format: { with: /\A(?!.*(.)\1\1)(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{10,16}\z/ }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save user without name" do
+    user = User.new
+    assert_not user.save, "Saved the user without a name"
+  end
+
+  test "should not save user without password" do
+    user = User.new(name: "name", password: nil)
+    assert_not user.save, "Saved the user without a password"
+  end
+
+  test "password invalid if it is greater than 16" do
+    password = User.new(name: 'name', password: '01234567891234567')
+    assert_not password.valid?
+  end
+
+  test "password invalid if it is password less than 10" do
+    password = User.new(name: 'name', password: '012345678')
+    assert_not password.valid?
+  end
+
+  test "password invalid if it does not contain an uppercase" do
+    password = User.new(name: 'name', password: 'abcdefghijklmnop')
+    assert_not password.valid?
+  end
+
+  test "password invalid if it does not contain a digit" do
+    password = User.new(name: 'name', password: 'abcdefghijklmnop')
+    assert_not password.valid?
+  end
+
+  test "password invalid if it contains three repeating characters (eg: AAA)" do
+    password = User.new(name: 'name', password: 'AAAfk1swods')
+    assert_not password.valid?
+  end
+
+  test "password valid if it has at least 10 characters and at most 16 characters, 
+  it contains at least one lowercase character, one uppercase character and one digit,
+  It doesnt contain three repeating characters in a row" do
+    password = User.new(name: 'name', password: 'Aqpfk1swods')
+    assert password.valid?
+  end
 end
