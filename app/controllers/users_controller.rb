0eba6c7fe2a66e90796caf2bb0669ea6bdc492cb
Gergo Karcagi <karcagigergo@gmail.com>
2022-01-21 15:46:18 -0300

    now can upload a CSV - what creates a model only if it is valid. So the rest of the models are not saved and each of their errors (related to password) are shown on the index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,25 @@ class UsersController < ApplicationController
   end
 
   def import
-    User.my_import(params[:file])
-    redirect_to root_path, notice: "Successfully imported User data!"
+    my_import(params[:file])
+    redirect_to root_path
+  end
+
+  private
+
+  def my_import(file)
+    @users = []
+    CSV.foreach(file.path, headers: true) do |row|
+      @users << User.find_or_initialize_by(row.to_h)
+    end
+    User.import @users, on_duplicate_key_ignore: true
+    @users.each do |user|
+      if user.valid?
+        flash[:notice] = ["#{user.name} was successfully saved"]
+      else
+        flash[:notice] << "Change #{} character of #{user.name}'s password"
+      end
+    end
   end
 end
+# flash[:notice] = ["#{user.name} already exists in database."]
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   root to: "users#index"
-  get 'users/index'
-  get 'users/import' => 'users#my_import'
-  
   resources :users do
     collection {post :import}
   end
