8cf397e057b26c44590e03b11ccf1d4507fd2f79
Gergo Karcagi <karcagigergo@gmail.com>
2022-01-21 20:30:29 -0300

    some restructuring of the controller code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  include ActionView::Helpers::TextHelper
+
   def index
     @users = User.all
   end
@@ -12,29 +14,25 @@ class UsersController < ApplicationController
 
   def my_import(file)
     @users = []
-    CSV.foreach(file.path, headers: true) do |row|
+    flash[:notice] = []
+    CSV.foreach(file.path, headers: true, encoding: 'iso-8859-1:utf-8') do |row|
       @users << User.find_or_initialize_by(row.to_h)
     end
-    User.import @users, on_duplicate_key_ignore: true
     @users.each do |user|
-      if user.valid?
-        flash[:notice] = ["#{user.name} was successfully saved"]
+      if user.password.length < 10
+        difference = 10 - user.password.length
+        flash[:notice] << "Change #{pluralize(difference, 'character')} of #{user.name}'s password"
+      elsif user.password.length > 16
+        difference = user.password.length - 16
+        flash[:notice] << "Change #{pluralize(difference, 'character')} character of #{user.name}'s password"
+      elsif !(user.password.match(/(.*[a-z].*)/)) || !(user.password.match(/(.*[A-Z].*)/)) || !(user.password.match(/(.*\d.*)/))
+        flash[:notice] << "Change 1 character of #{user.name}'s password"
+      elsif user.password.scan(/((.)\2{2,})/).map(&:first).present?
+        flash[:notice] << "Change #{pluralize(user.password.scan(/((.)\2{2,})/).map(&:first).count, 'character')} of #{user.name}'s password"
       else
-        if user.password.length < 10
-          difference = 10 - user.password.length
-          flash[:notice] << "Change #{difference} character of #{user.name}'s password"
-        elsif user.password.length > 16
-          difference = user.password.length - 16
-          flash[:notice] << "Change #{difference} character of #{row['name']}'s password"
-        elsif !(user.password.match(/(.*[a-z].*)/)) || !(user.password.match(/(.*[A-Z].*)/)) || !(user.password.match(/(.*\d.*)/))
-          flash[:notice] << "Change 1 character of #{user.name}'s password"
-        elsif user.password.scan(/((.)\2{2,})/).map(&:first).present?
-          flash[:notice] << "Change #{user.password.scan(/((.)\2{2,})/).map(&:first).count} character of #{user.name}'s password"
-        else
-          flash[:notice] << "#{user.name} was successfully saved"
-        end
+        user.persisted? ? flash[:notice] << "Record with name: #{user.name} already exists!" : flash[:notice] << "#{user.name} was successfully saved"
       end
     end
+    User.import @users, on_duplicate_key_ignore: true # https://github.com/zdennis/activerecord-import#duplicate-key-ignore
   end
 end
-# flash[:notice] = ["#{user.name} already exists in database."]
